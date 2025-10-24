19ec9de3971a04ea9a453446b5c082c584ffb842
haichteque <147334486+haichteque@users.noreply.github.com>
2025-10-24 18:17:15 +0500

    Changes to test file

--- calculator.test.js
+++ calculator.test.js
@@ -1,15 +1,15 @@
 test('add 2 + 3 = 5', () => {
-    expect(functionCall(add(2,3))).toBe(5);
-  });
+    expect(add(2, 3)).toBe(5);
+});
 
-  test('subtract 3 - 2 = 1', () => {
-    expect(functionCall(subtract(3,2))).toBe(1);
-  });
+test('subtract 3 - 2 = 1', () => {
+    expect(subtract(3, 2)).toBe(1);
+});
 
-  test('multiply 4 * 6 = 24', () => {
-    expect(functionCall(multiply(4,6))).toBe(24);
-  });
+test('multiply 4 * 6 = 24', () => {
+    expect(multiply(4, 6)).toBe(24);
+});
 
-  test('divide 12 / 2 = 6', () => {
-    expect(functionCall(divide(12,2))).toBe(6);
-  });
+test('divide 12 / 2 = 6', () => {
+    expect(divide(12, 2)).toBe(6);
+});
