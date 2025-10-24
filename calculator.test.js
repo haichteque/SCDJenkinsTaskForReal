ec7dab4afd2095ffba3758452430979c121d52d7
haichteque <147334486+haichteque@users.noreply.github.com>
2025-10-24 18:21:04 +0500

    Further changes

--- calculator.test.js
+++ calculator.test.js
@@ -1,15 +1,18 @@
+
+const calculator = require('./calculator');
+
 test('add 2 + 3 = 5', () => {
-    expect(add(2, 3)).toBe(5);
+    expect(calculator.add(2, 3)).toBe(5);
 });
 
 test('subtract 3 - 2 = 1', () => {
-    expect(subtract(3, 2)).toBe(1);
+    expect(calculator.subtract(3, 2)).toBe(1);
 });
 
 test('multiply 4 * 6 = 24', () => {
-    expect(multiply(4, 6)).toBe(24);
+    expect(calculator.multiply(4, 6)).toBe(24);
 });
 
 test('divide 12 / 2 = 6', () => {
-    expect(divide(12, 2)).toBe(6);
+    expect(calculator.divide(12, 2)).toBe(6);
 });
